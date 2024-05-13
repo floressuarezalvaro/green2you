514d8f293a105326314c04ca8390632ff511d929
Alvy <floressuarezalvaro@gmail.com>
2024-05-13 12:25:56 -0400

    added hooks

---
+++ frontend/src/hooks/useSignUp.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { useAuthContext } from "./useAuthContext";
+
+export const useSignUp = () => {
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(null);
+  const { dispatch } = useAuthContext();
+
+  const signUp = async (email, password) => {
+    setIsLoading(true);
+    setError(null);
+
+    const response = await fetch("/users/signup", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
+    const json = await response.json();
+
+    if (!response.ok) {
+      setIsLoading(false);
+      setError(json.error);
+    }
+    if (response.ok) {
+      // save user to local storage
+      localStorage.setItem("user", JSON.stringify(json));
+
+      // update auth context
+      dispatch({ type: "LOGIN", payload: json });
+
+      setIsLoading(false);
+    }
+  };
+  return { signUp, isLoading, error };
+};
