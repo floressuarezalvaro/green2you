08c87189ae78fb6da17a7330cfaba58b54808ed0
Alvy <floressuarezalvaro@gmail.com>
2024-05-20 11:26:16 -0400

    removed console log for loading

--- frontend/src/context/authContext.js
+++ frontend/src/context/authContext.js
@@ -36,8 +36,6 @@ export const AuthContextProvider = ({ children }) => {
     setLoading(false);
   }, []);
 
-  console.log("AuthContext state:", state);
-
   if (loading) {
     return <div>Loading...</div>;
   }
