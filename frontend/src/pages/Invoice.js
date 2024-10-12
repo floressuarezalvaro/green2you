46a57f78d18b84ac6aa29b590a12076da0f62153
Alvy <floressuarezalvaro@gmail.com>
2024-10-12 10:58:24 -0400

    added log for apiUrl

--- frontend/src/pages/Invoice.js
+++ frontend/src/pages/Invoice.js
@@ -20,6 +20,7 @@ const Invoice = () => {
 
       try {
         const apiUrl = process.env.REACT_APP_API_URL;
+        console.log(apiUrl);
 
         const response = await fetch(`${apiUrl}/invoices`, {
           method: "GET",
