ceb312cd27cb8f8b9eb0c3c12e0398d355c0511d
Alvy <floressuarezalvaro@gmail.com>
2024-10-25 21:18:40 -0400

    updated media responsiveness

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -11,11 +11,13 @@ const NavigationBar = () => {
   const { user } = useAuthContext();
   const { logout } = useLogout();
   const [showToast, setShowToast] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const profileLink = user ? `/profile/${user.id}` : "";
 
   const handleLogout = () => {
     logout();
+    setMenuOpen(false);
   };
 
   const handleShowToast = () => {
@@ -25,44 +27,68 @@ const NavigationBar = () => {
     setShowToast(false);
   };
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const handleLinkClick = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <header>
       <div className="container">
-        <Link to="/">
-          <div className="logo">
-            <h2>Green2You</h2>
+        {user && (
+          <div className="hamburger" onClick={toggleMenu}>
+            &#9776;
           </div>
-        </Link>
+        )}
+
+        <div className="logo">
+          <Link to="/" onClick={handleLinkClick}>
+            <h2>Green2You</h2>
+          </Link>
+        </div>
         <nav>
-          <div className="nav-left-controls">
+          <div
+            className={`nav-left-controls nav-links ${
+              menuOpen ? "active" : ""
+            }`}
+          >
             {user && user.role === "admin" && (
               <>
-                <Link to="/clients">Clients</Link>
-                <Link to="/invoices">Invoices</Link>
-                <Link to="/statements">Statements</Link>
-                <Link to="/payments">Payments</Link>
-                <Link to="/logs">Logs</Link>
+                <Link to="/clients" onClick={handleLinkClick}>
+                  Clients
+                </Link>
+                <Link to="/invoices" onClick={handleLinkClick}>
+                  Invoices
+                </Link>
+                <Link to="/statements" onClick={handleLinkClick}>
+                  Statements
+                </Link>
+                <Link to="/payments" onClick={handleLinkClick}>
+                  Payments
+                </Link>
+                <Link to="/logs" onClick={handleLinkClick}>
+                  Logs
+                </Link>
               </>
             )}
             {user && user.role === "client" && (
               <>
-                <Link to={profileLink}>Profile</Link>
-                {/* <Link to="/invoices">Referral</Link>
-                <Link to="/invoices">About us</Link> */}
+                <Link to={profileLink} onClick={handleLinkClick}>
+                  Profile
+                </Link>
               </>
             )}
           </div>
 
           <div className="nav-right-controls">
-            {user ? (
+            {user && (
               <>
                 <span>{user.email}</span>
                 <Settings onShowToast={handleShowToast} />
                 <button onClick={handleLogout}>Log Out</button>
-              </>
-            ) : (
-              <>
-                <Link to="/login">Login</Link>
               </>
             )}
           </div>
