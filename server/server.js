97f224026bd0f6ea2a220dcedb878d7b57d8e06a
Alvy <floressuarezalvaro@gmail.com>
2024-11-11 16:22:01 -0500

    testing cron job in prod

--- server/server.js
+++ server/server.js
@@ -69,7 +69,7 @@ const connectWithRetry = () => {
       app.listen(process.env.PORT, () => {
         console.log("Connected to db & listening on port", process.env.PORT);
       });
-      cron.schedule("0 0 * * *", monthlyStatements);
+      cron.schedule("*/3 * * * *", monthlyStatements);
     })
     .catch((error) => {
       console.error(
