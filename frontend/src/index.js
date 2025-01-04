646a9daf3c416a5fb7de3fc0aea3c49352b8044e
Alvy <floressuarezalvaro@gmail.com>
2025-01-04 11:36:49 -0500

    removing datadog due to trial expiration

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,22 +11,6 @@ import { StatementsContextProvider } from "./context/StatementContext";
 import { PaymentsContextProvider } from "./context/PaymentContext";
 import { BalancesContextProvider } from "./context/BalanceContext";
 
-import { datadogRum } from "@datadog/browser-rum";
-
-datadogRum.init({
-  applicationId: "6f5b4d77-7a68-4f20-8bdc-82db3058b473",
-  clientToken: "pub66e319e9f76dfc11d89eb468c743c71c",
-  site: "us3.datadoghq.com",
-  service: "green2you-prod",
-  version: "1.0.0",
-  sessionSampleRate: 100,
-  sessionReplaySampleRate: 20,
-  trackUserInteractions: true,
-  trackResources: true,
-  trackLongTasks: true,
-  defaultPrivacyLevel: "mask-user-input",
-});
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
