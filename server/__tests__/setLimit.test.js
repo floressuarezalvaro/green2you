0c245674241e2a22b61a1707cb60014911c4af47
Alvy <floressuarezalvaro@gmail.com>
2025-10-15 12:13:10 -0400

    Added setLimit test and removed dummy tests

---
+++ server/__tests__/setLimit.test.js
@@ -0,0 +1,44 @@
+const rateLimit = require("express-rate-limit");
+const setLimit = require("../utils/setLimit.js");
+
+jest.mock("express-rate-limit");
+
+describe("Tests the API rate limiting function", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("sets default limit", () => {
+    setLimit();
+    expect(rateLimit).toHaveBeenCalledTimes(1);
+    expect(rateLimit).toHaveBeenCalledWith({
+      windowMs: 10 * 60 * 1000,
+      limit: 50,
+      message: {
+        error: "Limit maxed out. Try again later.",
+      },
+      standardHeaders: false,
+      legacyHeaders: false,
+      ipv6Subnet: 56,
+      skipSuccessfulRequests: true,
+    });
+  });
+
+  test("sets custom limit", () => {
+    const customLimit = 10;
+    setLimit(customLimit);
+
+    expect(rateLimit).toHaveBeenCalledTimes(1);
+    expect(rateLimit).toHaveBeenCalledWith({
+      windowMs: 10 * 60 * 1000,
+      limit: 10,
+      message: {
+        error: "Limit maxed out. Try again later.",
+      },
+      standardHeaders: false,
+      legacyHeaders: false,
+      ipv6Subnet: 56,
+      skipSuccessfulRequests: true,
+    });
+  });
+});
